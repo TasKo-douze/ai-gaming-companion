54613f68e301d566811fd55469bd94b623f8425f
TasKo-douze <matyas.kncsk@eduge.ch>
2026-06-02 19:35:43 +0200

    fix(goals): handle COME_HERE and STAY_HERE intents in createGoalFromIntent and include username metadata

--- src/goals/GoalManager.ts
+++ src/goals/GoalManager.ts
@@ -28,6 +28,10 @@ export class GoalManager {
         return this.createGoal(GoalType.FOLLOW_PLAYER, 10, { username });
       case Intent.STOP_FOLLOWING:
         return this.createGoal(GoalType.STOP_FOLLOWING, 20, { username });
+      case Intent.COME_HERE:
+        return this.createGoal(GoalType.COME_HERE, 15, { username });
+      case Intent.STAY_HERE:
+        return this.createGoal(GoalType.STAY_HERE, 20, { username });
       case Intent.HELP:
         return this.createGoal(GoalType.IDLE, 0, { reason: 'help' });
       case Intent.GREETING:
